280d6ef6d944740189710ed4e284fc733a4651a7
oalles <oalles@gmail.com>
2015-09-21 16:00:42 +0200

    Exception to raise in case an invalid configuration is provided

--- src/main/java/es/neivi/mtc/exceptions/InvalidMTCConfiguration.java
+++ src/main/java/es/neivi/mtc/exceptions/InvalidMTCConfiguration.java
@@ -4,4 +4,30 @@ package es.neivi.mtc.exceptions;
  * The configuration provided for the MTC component is INVALID.
  */
 public class InvalidMTCConfiguration extends MTCException {
+
+	public InvalidMTCConfiguration() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidMTCConfiguration(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidMTCConfiguration(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidMTCConfiguration(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidMTCConfiguration(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
 }
