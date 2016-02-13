5273b893cf29dc57828a30ff9bc3571017c76d6c
oalles <oalles@neivi.es>
2016-02-13 21:10:32 +0100

    Removing TODO tags

--- src/main/java/es/omarall/mtc/exceptions/CappedCollectionRequiredException.java
+++ src/main/java/es/omarall/mtc/exceptions/CappedCollectionRequiredException.java
@@ -6,28 +6,22 @@ package es.omarall.mtc.exceptions;
 public class CappedCollectionRequiredException extends MTCException {
 	
 	public CappedCollectionRequiredException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public CappedCollectionRequiredException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 	public CappedCollectionRequiredException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public CappedCollectionRequiredException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public CappedCollectionRequiredException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
-
 }
--- src/main/java/es/omarall/mtc/exceptions/NotStartedException.java
+++ src/main/java/es/omarall/mtc/exceptions/NotStartedException.java
@@ -7,28 +7,22 @@ package es.omarall.mtc.exceptions;
 public class NotStartedException extends MTCException {
 
 	public NotStartedException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public NotStartedException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NotStartedException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NotStartedException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NotStartedException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
-
 }
